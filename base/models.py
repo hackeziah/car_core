8f5fa0030b8643f4c773f5ff674a982c48086718
hackeziah <lamadridkevinpaul@gmail.com>
2023-02-07 16:40:20 +0800

    refactoring

--- base/models.py
+++ base/models.py
@@ -9,8 +9,8 @@ class BaseManager(models.Manager):
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created = models.DateField(auto_now_add=True)
-    lastUpdated = models.DateField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    lastUpdated = models.DateTimeField(auto_now=True)
     isDeleted = models.BooleanField(default=False)
 
     class Meta:
